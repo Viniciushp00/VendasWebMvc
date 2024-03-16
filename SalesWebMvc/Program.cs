315e8dfccd1c20d940b402bbb3db151bfc0947b2
Marcos Vinicius <viniciushp00@gmail.com>
2024-03-16 00:47:56 -0300

    Configurando SQLServer e primeira Migration

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -9,7 +9,8 @@ namespace SalesWebMvc
         {
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddDbContext<SalesWebMvcContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("SalesWebMvcContext") ?? throw new InvalidOperationException("Connection string 'SalesWebMvcContext' not found.")));
+                options.UseSqlServer(builder.Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
+                        builder.MigrationsAssembly("SalesWebMvc")));
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
