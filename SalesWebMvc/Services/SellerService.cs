4acbab4a9defbf6e89a760a0d59747c230071ab3
Marcos Vinicius <viniciushp00@gmail.com>
2024-03-20 19:50:31 -0300

    Criação da ação Update Seller e criação de Exceptions

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
 using SalesWebMvc.Models;
+using SalesWebMvc.Services.Exceptions;
 
 namespace SalesWebMvc.Services
 {
@@ -20,7 +21,7 @@ namespace SalesWebMvc.Services
 
         public Seller FindById(int id)
         {
-            //Include colocado para fazer o Join com a tabela de departamento
+            //Include colocado para fazer o Join com a tabela de departamento (Eager Loading)
             return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
@@ -34,6 +35,23 @@ namespace SalesWebMvc.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException ex)
+            {
+                throw new DbConcurrencyException(ex.Message);
+            }
         }
     }
 }
